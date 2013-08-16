f37f44e38e0527887ec53b5e5c44858433125dc7
toledoalbert <toledoalbert@gmail.com>
2013-08-16 17:14:24 -0400

    turning label plus detail form experiment works

--- PartExperiments/PopUpNewLabel/newLabel.js
+++ PartExperiments/PopUpNewLabel/newLabel.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
 
+	$('#details').hide();
+
+	$('#flip').click(function() {
+
+
+		$("#flipBox").flip({
+			direction:'rl',
+
+			onEnd: function(){
+
+				if($('#flip').text() == 'Details'){
+
+					$('#label').hide();
+					$('#details').show();
+					$('#flip').text('Label');
+
+				}else if($('#flip').text() == 'Label'){
+
+					$('#details').hide();
+					$('#label').show();
+					$('#flip').text('Details');
+					
+				}
+
+			}
+		})
+
+
+	});
+
 	
 
 });
